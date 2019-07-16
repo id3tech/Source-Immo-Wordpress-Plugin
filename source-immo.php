7e9c4690c9e99d50d520a7bd27c25a9cb040e778
Marc-André Labonté <vatero@id-3.net>
2019-07-16 12:41:31 -0400

    Auto-update

--- source-immo.php
+++ source-immo.php
@@ -3,7 +3,7 @@
 Plugin Name: Source.immo
 Plugin URI: https://source.immo
 Description: Connect to your Source.immo account and display your real estate venues with easy tools and shortcodes
-Version: 0.1.31
+Version: 0.2.0
 Author: ID-3 Technologies
 Author URI: https://id-3.net/source-immo
 License: GPLv2 or later
@@ -12,7 +12,7 @@ Text Domain: si
 define( 'SI_NAME', 'Source.immo');
 define( 'SI', 'si' );
 define( 'SI_APP_ID', 'ead7575f-8d1c-42e7-9f59-4cf9e065167e');
-define( 'SI_VERSION', '0.1.31 BETA' );
+define( 'SI_VERSION', '0.2.0 BETA' );
 define( 'SI_MINIMUM_WP_VERSION', '4.0' );
 define( 'SI_PLUGIN', __FILE__);
 define( 'SI_PLUGIN_DIR', str_replace('\\', '/',plugin_dir_path( __FILE__ ) ) );
@@ -34,6 +34,11 @@ add_action( 'rest_api_init', array( 'SourceImmoAPI', 'init' ) );
 if ( is_admin() || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
 	require_once( SI_PLUGIN_DIR . '/core/class.si-admin.php' );
 	add_action( 'init', array( 'SourceImmoAdmin', 'init' ) );
+}
+
+if ( is_admin() ) {
+	require_once( SI_PLUGIN_DIR . '/core/class.si-update.php' );
+    new SiGitHubPluginUpdater( __FILE__, 'id3tech', "Source-Immo-Wordpress-Plugin","a34c5dc887337c3b8fd38f69c59e5b01cab03e53" );
 }
 
 load_default_textdomain();
