c4346a32c531aef7289ffd2c582301990409ede3
Marc-André Labonté <vatero@id-3.net>
2020-07-29 11:08:46 -0400

    Avada fatal error fix

--- modules/avada/functions.php
+++ modules/avada/functions.php
@@ -1,7 +1,9 @@
 <?php
 function avada_page_builder_prerender($page_id){
     if(class_exists('Avada_Dynamic_CSS')){
-        wp_enqueue_style( 'avada-dynamic-css', Avada_Dynamic_CSS::file( 'uri' ), array( 'avada-stylesheet' ) );
+        if(method_exists('Avada_Dynamic_CSS','file')){
+            wp_enqueue_style( 'avada-dynamic-css', Avada_Dynamic_CSS::file( 'uri' ), array( 'avada-stylesheet' ) );
+        }
     }
 }
 add_action('si_page_builder_prerender', 'avada_page_builder_prerender');
